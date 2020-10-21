1cdcd0c8d2c96b6678b8ffc2dc869b057164d479
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-10-20 20:08:50 -0700

    user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :rfc, presence: true, uniqueness: true, length: { minimum: 12, maximum: 13 }
+  validates :company, presence: true
 end
