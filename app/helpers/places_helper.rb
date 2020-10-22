ed0170d58682137f2ad72e4b508467223c3af1e0
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-10-21 19:44:22 -0700

    partial to show places and links to create places

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,9 @@
 module PlacesHelper
+  def render_places
+    if @places.count.positive?
+      render 'places/places'
+    else
+      render html: "<p>No hay sucursales registradas<p/>".html_safe
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :places
-  root to: 'static_pages#index'
+  root to: 'places#index'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'new_account' },
              controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
